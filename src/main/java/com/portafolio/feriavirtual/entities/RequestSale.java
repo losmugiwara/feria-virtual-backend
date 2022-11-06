0d813671e238534fe3d4745f118fedbeddbdd982
furibe_vt <furibe@vtsystems.cl>
2022-11-06 00:23:16 -0300

    update

--- src/main/java/com/portafolio/feriavirtual/entities/RequestSale.java
+++ src/main/java/com/portafolio/feriavirtual/entities/RequestSale.java
@@ -10,7 +10,6 @@ import lombok.Setter;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
