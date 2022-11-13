ef7630bfaeade0a9707b37bcee8514626f72612d
Francisco Uribe <franciscouribematus@gmail.com>
2022-11-13 20:36:02 -0300

    cors en contracts

--- src/main/java/com/portafolio/feriavirtual/controllers/ContractController.java
+++ src/main/java/com/portafolio/feriavirtual/controllers/ContractController.java
@@ -3,14 +3,17 @@ package com.portafolio.feriavirtual.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.portafolio.feriavirtual.dao.ContractDao;
 
 @RestController
 @RequestMapping("/api/contracts")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class ContractController {
 
     @Autowired
